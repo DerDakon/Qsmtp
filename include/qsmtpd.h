edc906445f352cf35ec3b1e955258b05d29a8263
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2004-10-13 16:42:03 +0000

    -add xmitstat.helostatus to let the users decide on which broken helos they want to reject mail -define size of spf and fromdomain in xmitstat to save some bytes of memory -add comment to helostr to make sure everyone gets the point that this may be legally be NULL -make heloname visible from other files -add xmitstat.ipv4conn, it's just the result of IN6_IS_ADDR_V4MAPPED(xmitstat.sremoteip)
    svn path=/; revision=13

--- include/qsmtpd.h
+++ include/qsmtpd.h
@@ -9,6 +9,10 @@ struct xmitstat {			/* This contains some flags describing the transmission and 
 					 * This can be passed to the usercallbacks */
 	unsigned int esmtp:1;		/* if we are using ESMTP extensions */
 	unsigned int check2822:2;	/* if or not to check the message to strict compliance to RfC 2822 */
+	unsigned int helostatus:3;	/* status of the given HELO/EHLO, see antispam.h for the meaning */
+	int spf:4;			/* result of the SPF lookup */
+	int fromdomain:3;		/* result of the lookup for fromips */
+	unsigned int ipv4conn:1;	/* if this connection is made from a real IPv6 address or not */
 	unsigned long thisbytes;	/* size of the message announced by the remote host */
 	string mailfrom;		/* the current from address */
 	char *authname;			/* if SMTP AUTH is used (and successful) this is set */
@@ -16,11 +20,10 @@ struct xmitstat {			/* This contains some flags describing the transmission and 
 	string remotehost;		/* the reverse lookup of the remote host */
 	char *remoteip;			/* ip of the remote host as set in the environment */
 	char *remoteinfo;		/* info gathered by tcpserver like remote username */
-	string helostr;			/* the helo string sent by the client if different from the reverse lookup */
+	string helostr;			/* the helo string sent by the client if different from the reverse lookup
+					 * if the helo is identical to remotehost this is {NULL, 0} */
 	struct in6_addr sremoteip;	/* parsed remoteip */
 	struct ips *frommx;		/* MX IPs of from domain */
-	int fromdomain;			/* result of the lookup for fromips */
-	int spf;			/* result of the SPF lookup */
 };
 
 extern struct xmitstat xmitstat;
@@ -28,6 +31,7 @@ extern char *protocol;
 extern char *auth_host;			/* hostname for auth */
 extern char *auth_check;		/* checkpassword or one of his friends for auth */
 extern char **auth_sub;			/* subprogram and arguments to be invoked by auth_check (usually /bin/true) */
+extern char *heloname;			/* the fqdn to show in helo */
 
 extern int err_control(const char *);
 
