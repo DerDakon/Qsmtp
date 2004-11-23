c7795e82240418f45403ab1e69aa4239c3c28e06
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2004-11-23 11:29:03 +0000

    add noreturn to ssl_exit and do some coding style fixes
    svn path=/; revision=105

--- lib/tls.c
+++ lib/tls.c
@@ -7,22 +7,25 @@
 SSL *ssl = NULL;
 
 void ssl_free(SSL *myssl) { SSL_shutdown(myssl); SSL_free(myssl); }
-void ssl_exit(int status) { if (ssl) ssl_free(ssl); _exit(status); }
+void __attribute__ ((noreturn)) ssl_exit(int status) { if (ssl) ssl_free(ssl); _exit(status); }
 
 const char *ssl_error(void)
 {
 	int r = ERR_get_error();
+
 	if (!r)
 		return NULL;
 	SSL_load_error_strings();
 	return ERR_error_string(r, NULL);
 }
+
 const char *ssl_strerror(void)
 {
 	const char *err = ssl_error();
+
 	if (err)
 		return err;
 	if (!errno)
-		return 0;
+		return NULL;
 	return errno == ETIMEDOUT ? "timed out" : strerror(errno);
 }
