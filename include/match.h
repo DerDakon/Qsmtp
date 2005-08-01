3f0966d3770328eb8bc4154b52b9180b6e2ae1ff
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2005-08-01 09:42:10 +0000

    add nonnull attributes for matching functions
    svn path=/trunk/; revision=720

--- include/match.h
+++ include/match.h
@@ -4,8 +4,8 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
-extern int ip4_matchnet(const struct in6_addr *, const struct in_addr *, const unsigned char);
-extern int ip6_matchnet(const struct in6_addr *, const struct in6_addr *, const unsigned char);
-extern int matchdomain(const char *, const size_t, const char *);
+extern int ip4_matchnet(const struct in6_addr *, const struct in_addr *, const unsigned char) __attribute__ ((nonnull (1,2)));
+extern int ip6_matchnet(const struct in6_addr *, const struct in6_addr *, const unsigned char) __attribute__ ((nonnull (1,2)));
+extern int matchdomain(const char *, const size_t, const char *) __attribute__ ((nonnull (1,3)));
 
 #endif
