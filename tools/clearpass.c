191c03300d12efc654a4cd3e0d10d63ae3fb502a
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2010-06-30 09:52:54 +0000

    clearpass: check for base64 errors
    svn path=/trunk/; revision=916

--- tools/clearpass.c
+++ tools/clearpass.c
@@ -10,12 +10,16 @@
 int main(int argc, char *argv[])
 {
 	string clear;
+	int err;
 	size_t i;
 
 	if (argc == 1)
 		return 1;
 
-	b64decode(argv[1], strlen(argv[1]), &clear);
+	err = b64decode(argv[1], strlen(argv[1]), &clear);
+	if (err != 0)
+		return err;
+
 	for (i = 0; i < clear.len; i++) {
 		if (!clear.s[i]) {
 			write(1, "\\0", 2);
