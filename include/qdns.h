73117997b7d1c44caabae6661bee18d05a552475
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2010-07-29 10:48:19 +0000

    remove bogus nonnull attribute from freeips()
    This causes gcc to optimize the first check away in release builds and then crash. In fact it's perfectly valid to pass a NULL pointer here.
    
    svn path=/trunk/; revision=934

--- include/qdns.h
+++ include/qdns.h
@@ -29,7 +29,7 @@ extern int ask_dnsmx(const char *, struct ips **) __attribute__ ((nonnull (1,2))
 extern int ask_dnsaaaa(const char *, struct ips **) __attribute__ ((nonnull (1,2)));
 extern int ask_dnsa(const char *, struct ips **) __attribute__ ((nonnull (1)));
 extern int ask_dnsname(const struct in6_addr *, char **) __attribute__ ((nonnull (1,2)));
-extern void freeips(struct ips *) __attribute__ ((nonnull (1)));
+extern void freeips(struct ips *);
 extern int domainvalid(const char *) __attribute__ ((pure)) __attribute__ ((nonnull (1)));
 extern void sortmx(struct ips **p) __attribute__ ((nonnull (1)));
 
