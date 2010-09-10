d127a02d755db0335089077343b0699240110b80
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2010-09-10 14:46:22 +0000

    tests: add more tests for different authtypes configurations
    svn path=/trunk/; revision=950

--- tests/authsetup_test.c
+++ tests/authsetup_test.c
@@ -20,22 +20,14 @@ char *auth_host = NULL;
 char *auth_check = NULL;
 char **auth_sub = NULL;
 
-#ifdef AUTHCRAM
-static const char *auth_expect = " LOGIN PLAIN CRAM-MD5";
-#else /* AUTHCRAM */
-static const char *auth_expect = " LOGIN PLAIN";
-#endif /* AUTHCRAM */
+static const char loginonly[] = " LOGIN";
+static const char plainonly[] = " PLAIN";
+static const char loginplain[] = " LOGIN PLAIN";
 
-int main(void)
+static int
+check_authstr(const char *auth_expect)
 {
-	int errcnt = 0;
-	char *auth_str;
-
-	memset(&xmitstat, 0, sizeof(xmitstat));
-	memset(linein, 0, sizeof(linein));
-	linelen = 0;
-
-	auth_str = smtp_authstring();
+	char *auth_str = smtp_authstring();
 
 	if (auth_str == NULL) {
 		if (errno == 0) {
@@ -48,9 +40,9 @@ int main(void)
 	}
 
 	if (strcmp(auth_str, auth_expect) != 0) {
-		const char msg1 = "smtp_authstring() returned \"";
-		const char msg2 = "\" instead of \"";
-		const char msg3 = "\"\n";
+		const char msg1[] = "smtp_authstring() returned \"";
+		const char msg2[] = "\" instead of \"";
+		const char msg3[] = "\"\n";
 
 		write(2, msg1, strlen(msg1));
 		write(2, auth_str, strlen(auth_str));
@@ -58,8 +50,142 @@ int main(void)
 		write(2, auth_expect, strlen(auth_expect));
 		write(2, msg3, strlen(msg3));
 
+		free(auth_str);
 		return 1;
 	}
+
+	free(auth_str);
+	return 0;
+}
+
+static int
+test_nocontrol(void)
+{
+#ifdef AUTHCRAM
+	static const char auth_expect[] = " LOGIN PLAIN CRAM-MD5";
+#else /* AUTHCRAM */
+	static const char *auth_expect = loginplain;
+#endif /* AUTHCRAM */
+
+	return check_authstr(auth_expect);
+}
+
+static int
+test_controlfiles(void)
+{
+	struct {
+		const char *subdir;
+		const char *expect;
+	} patterns[] = {
+		{
+			.subdir = "login_only",
+			.expect = loginonly
+		},
+		{
+			.subdir = "plain_only",
+			.expect = plainonly
+		},
+		{
+			.subdir = "login_plain",
+			.expect = loginplain
+		},
+		{
+			.subdir = "duplicate_plain",
+			.expect = plainonly
+		},
+		{
+			.subdir = NULL,
+			.expect = NULL
+		}
+	};
+	unsigned int idx;
+	int errcnt = 0;
+
+	while (patterns[idx].subdir != NULL) {
+		if (chdir(patterns[idx].subdir) != 0) {
+			const char *errmsg = "cannot chdir() to ";
+			write(2, errmsg, strlen(errmsg));
+			write(2, patterns[idx].subdir, strlen(patterns[idx].subdir));
+			write(2, "\n", 1);
+			errcnt++;
+			idx++;
+			continue;
+		}
+
+		if (check_authstr(patterns[idx].expect) != 0)
+			errcnt++;
+
+		if (chdir("..") != 0) {
+			const char *errmsg = "cannot chdir() to back to start dir\n";
+			write(2, errmsg, strlen(errmsg));
+			errcnt++;
+			idx++;
+			continue;
+		}
+		idx++;
+	}
+
+	return errcnt;
+}
+
+static int
+test_nonexistent(void)
+{
+	char *auth_str;
+
+	if (chdir("nonexistent") != 0) {
+		const char *errmsg = "cannot chdir() to \"nonexistent\"\n";
+		write(2, errmsg, strlen(errmsg));
+		return 1;
+	}
+
+	auth_str = smtp_authstring();
+
+	if (auth_str != NULL) {
+		const char errmsg1[] = "smtp_authstring() returned \"";
+		const char errmsg2[] = "\" but is should have returned NULL\n";
+		write(2, errmsg1, strlen(errmsg1));
+		write(2, auth_str, strlen(auth_str));
+		write(2, errmsg2, strlen(errmsg2));
+		free(auth_str);
+		return 1;
+	}
+
+	if (chdir("..") != 0) {
+		const char *errmsg = "cannot chdir() to back to start dir\n";
+		write(2, errmsg, strlen(errmsg));
+		return 1;
+	}
+
+	return 0;
+}
+
+
+int main(int argc, char **argv)
+{
+	int errcnt = 0;
+
+	if (argc != 2) {
+		const char *errmsg = "required argument missing: base directory for control file tests\n";
+		write(2, errmsg, strlen(errmsg));
+		return EINVAL;
+	}
+
+	if (chdir(argv[1]) != 0) {
+		const char *errmsg = "cannot chdir() to given directory\n";
+		write(2, errmsg, strlen(errmsg));
+		return -1;
+	}
+
+	memset(&xmitstat, 0, sizeof(xmitstat));
+	memset(linein, 0, sizeof(linein));
+	linelen = 0;
+
+	if (test_nocontrol() != 0)
+		errcnt++;
+
+	errcnt += test_controlfiles();
+	errcnt += test_nonexistent();
 
 	return 0;
 }
