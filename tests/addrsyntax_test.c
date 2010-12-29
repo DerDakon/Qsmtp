845f29d55970a2de498f40b8f7f87cbea2571bd6
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2010-12-29 17:16:58 +0000

    AddrSyntax test: add more test expressions
    svn path=/trunk/; revision=1029

--- tests/addrsyntax_test.c
+++ tests/addrsyntax_test.c
@@ -12,13 +12,31 @@ const char *valid[] = {
 	"\"foo\"@bar.example.com",
 	"\"f\\\"oo\"@bar.example.com",
 	"\"f\\\\oo\"@bar.example.com",
+	"me@[127.0.0.1]",
+	"me@[IPv6:::1]",
+	"me@[IPv6:ffe::ffff:0123]",
 	NULL
 };
 
 const char *invalid[] = {
-	"fo.\nbar@example.com",
-	"foo@bar..example.com",
-	"\"f\\oo\"@bar.example.com",
+	"fo.\nbar@example.com",		/* unquoted control character */
+	"foo@bar..example.com",		/* double dot in domainpart */
+	"\"f\\oo\"@bar.example.com",	/* invalid quoting */
+	"\"foo@example.com",		/* unterminated quote */
+	"toolonglocalpart12345678901234567890123456789012345678901234567890@example.com",	/* localpart too long */
+	"me@[127.0.0.256]",		/* invalid IPv4 address */
+	"me@[IPv6:::abcd:fg:0:8:9]",	/* invalid IPv6 address (invalid hex character 'g') */
+	"me@[IPv6:::abcd::1234:2]",	/* invalid IPv6 address (multiple ::) */
+	"me@[::1]",			/* valid IPv6 address without IPv6: prefix */
+	"me@[IPv6:::1",			/* missing closing bracket */
+	"me@[IPv6:::1].com",		/* text after closing bracket */
+	NULL
+};
+
+/* not valid as email address, but as filter expression */
+const char *validparts[] = {
+	"foo.example.com",		/* only domain */
+	"@foo.example.com",		/* missing localpart */
 	NULL
 };
 
@@ -64,6 +82,27 @@ main(void)
 			err++;
 			fputs("addrspec_valid() did not reject invalid address: ", stderr);
 			fputs(invalid[i], stderr);
+			fputs("\n", stderr);
+		}
+
+		i++;
+	}
+
+	i = 0;
+	while (validparts[i] != NULL) {
+		fputs("testing valid address part: ", stdout);
+		puts(validparts[i]);
+		if (checkaddr(validparts[i]) != 0) {
+			err++;
+			fputs("checkaddr() rejected valid filter expression: ", stderr);
+			fputs(validparts[i], stderr);
+			fputs("\n", stderr);
+		}
+
+		if (addrspec_valid(validparts[i])) {
+			err++;
+			fputs("addrspec_valid() did not reject invalid address: ", stderr);
+			fputs(validparts[i], stderr);
 			fputs("\n", stderr);
 		}
 
