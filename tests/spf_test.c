8e36eda9477b39b25972a2d2aa60a8b08358ed15
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2011-01-01 17:50:18 +0000

    SPF test: cover more basic SPF states
    svn path=/trunk/; revision=1080

--- tests/spf_test.c
+++ tests/spf_test.c
@@ -300,42 +300,68 @@ test_parse()
 			.value = "v=spf1  "
 		},
 		{
+			.type = DNSTYPE_TXT,
+			.key = "allneutral.example.net",
+			.value = "v=spf1 ?all"
+		},
+		{
+			.type = DNSTYPE_TXT,
+			.key = "allfail.example.net",
+			.value = "v=spf1 -all"
+		},
+		{
+			.type = DNSTYPE_TXT,
+			.key = "allsoftfail.example.net",
+			.value = "v=spf1 ~all"
+		},
+		{
+			.type = DNSTYPE_TXT,
+			.key = "allpass.example.net",
+			.value = "v=spf1 +all"
+		},
+		{
+			.type = DNSTYPE_TXT,
+			.key = "allpassnoprefixupcase.example.net",
+			.value = "v=spf1 ALL"
+		},
+		{
+			.type = DNSTYPE_TXT,
+			.key = "invalidchar.example.net",
+			.value = "v=spf1 !ALL"
+		},
+		{
+			.type = DNSTYPE_TXT,
+			.key = "redirect-softfail.example.net",
+			.value =" v=spf1 redirect=allsoftfail.example.net"
+		},
+		{
 			.type = DNSTYPE_NONE,
 			.key = NULL,
 			.value = NULL
 		}
 	};
+	static int spfresults[] = {
+		SPF_HARD_ERROR,
+		SPF_FAIL_NONEX,
+		SPF_NONE,
+		SPF_HARD_ERROR,
+		SPF_NEUTRAL,
+		SPF_NEUTRAL,
+		SPF_FAIL_PERM,
+		SPF_SOFTFAIL,
+		SPF_PASS,
+		SPF_PASS,
+		SPF_HARD_ERROR,
+		SPF_SOFTFAIL
+	};
 	int err = 0;
+	unsigned int i = 0;
 	int r;
 
 	dnsdata = parseentries;
-	r = check_host(parseentries[0].key);
-	if (r != SPF_HARD_ERROR) {
-		fprintf(stderr, "check_host() on endless recursive entry did not fail with SPF_HARD_ERROR, but %i\n", r);
-		err++;
-	}
-
-	r = check_host(parseentries[1].key);
-	if (r != SPF_FAIL_NONEX) {
-		fprintf(stderr, "check_host() with broken include did not fail with SPF_FAIL_NONEX, but %i\n", r);
-		err++;
-	}
-
 	r = check_host("nonexistent.example.org");
 	if (r != SPF_NONE) {
 		fprintf(stderr, "check_host() without SPF entry SPF_NONE, but %i\n", r);
-		err++;
-	}
-
-	r = check_host(parseentries[2].key);
-	if (r != SPF_NONE) {
-		fprintf(stderr, "check_host() with non-SPF TXT entry did not return SPF_NONE, but %i\n", r);
-		err++;
-	}
-
-	r = check_host(parseentries[3].key);
-	if (r != SPF_HARD_ERROR) {
-		fprintf(stderr, "check_host() with invalid SPF entry (2 v=spf1 specifications) did not fail with SPF_HARD_ERROR, but %i\n", r);
 		err++;
 	}
 
@@ -345,10 +371,13 @@ test_parse()
 		err++;
 	}
 
-	r = check_host(parseentries[4].key);
-	if (r != SPF_NEUTRAL) {
-		fprintf(stderr, "check_host() with empty spec should return SPF_NEUTRAL, but did %i\n", r);
-		err++;
+	while (parseentries[i].key != NULL) {
+		r = check_host(parseentries[i].key);
+		if (r != spfresults[i]) {
+			fprintf(stderr, "check_host() for test %s should return %i, but did %i\n", parseentries[i].key, spfresults[i], r);
+			err++;
+		}
+		i++;
 	}
 
 	return err;
