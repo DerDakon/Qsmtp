9640bafe1a42e760fbc5caa6e42336ebe25d21ae
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2011-09-20 18:06:50 +0000

    Qremote: revert accidentially checked in code
    svn path=/trunk/; revision=1322

--- qremote/qremote.c
+++ qremote/qremote.c
@@ -282,14 +282,14 @@ checkreply(const char *status, const char **pre, const int mask)
 	if (status) {
 		int m;
 
-		if ((res >= 200) && (res <= 299)) {
+		if ((res >= 211) && (res <= 252)) {
 			if (status[0] == ' ') {
 				ignore = 1;
 			} else {
 				write(1, status, 1);
 			}
 			m = 1;
-		} else if ((res >= 400) && (res <= 499)) {
+		} else if ((res >= 421) && (res <= 452)) {
 			write(1, status + 1, 1);
 			m = 2;
 		} else {
@@ -486,8 +486,6 @@ main(int argc, char *argv[])
 /* for all MX entries we got: try to enable connection, check if the SMTP server wants us
  * (sends 220 response) and EHLO/HELO succeeds. If not, try next. If none left, exit. */
 	do {
-		int flagerr = 0;
-
 /*		if (i < 0) {
 			if (errno == ENOMEM)
 				err_mem(1);
@@ -502,28 +500,7 @@ main(int argc, char *argv[])
 			quitmsg();
 			continue;
 		}
-		while (!strncmp("220-", linein, 4)) {
-			if (net_read()) {
-				flagerr = 1;
-				switch (errno) {
-					case ENOMEM:	err_mem(1);
-					case EINVAL:
-					case E2BIG:	(void) write(1, "Zsyntax error in server reply\n", 31);
-							quitmsg();
-					default:	{
-							char *tmp = strerror(errno);
-
-							write(1, "Z", 1);
-							write(1, tmp, strlen(tmp) + 1);
-							quitmsg();
-					}
-				}
-			}
-		}
-		if (flagerr)
-			continue;
-
-		if (strncmp("220 ", linein, 4)) {
+		if (linein[3] != ' ') {
 			const char *dropmsg[] = {"invalid greeting from ", NULL, NULL};
 
 			getrhost(mx);
