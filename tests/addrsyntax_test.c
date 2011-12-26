67e123fc5bc8706cea4bd9a6f61996e850b6256a
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2011-12-26 14:35:20 +0000

    add some tests for addrsyntax()
    svn path=/trunk/; revision=1343

--- tests/addrsyntax_test.c
+++ tests/addrsyntax_test.c
@@ -1,7 +1,9 @@
 #include "qdns.h"
+#include "sstring.h"
 #include "test_io/testcase_io.h"
 
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 const char *valid[] = {
@@ -43,6 +45,122 @@ const char *validparts[] = {
 	"@foo.example.com",		/* missing localpart */
 	NULL
 };
+
+struct {
+	const char *in;
+	int flags;
+	const char *addr;
+	const char *more;
+	int retval;
+} syntaxpatterns[] = {
+	/* postmaster is only valid for flags == 1 */
+	{
+		.in = "postmaster>",
+		.flags = 0,
+		.addr = "",
+		.more = "",
+		.retval = 0
+	},
+	{
+		.in = "postmaster>",
+		.flags = 1,
+		.addr = "postmaster",
+		.more = "",
+		.retval = 1
+	},
+	/* postmaster is only valid for flags == 1 */
+	{
+		.in = "postmaster>",
+		.flags = 2,
+		.addr = "",
+		.more = "",
+		.retval = 0
+	},
+	{
+		.in = ">",
+		.flags = 0,
+		.addr = "",
+		.more = "",
+		.retval = 1
+	},
+	/* empty address is only valid for flags == 0 */
+	{
+		.in = ">",
+		.flags = 1,
+		.addr = "",
+		.more = "",
+		.retval = 0
+	},
+	/* empty address is only valid for flags == 0 */
+	{
+		.in = ">",
+		.flags = 2,
+		.addr = "",
+		.more = "",
+		.retval = 0
+	},
+	/* an ordinary address */
+	{
+		.in = "foo@bar.example.com>",
+		.flags = 0,
+		.addr = "foo@bar.example.com",
+		.more = "",
+		.retval = 3
+	},
+	/* an ordinary address with following text*/
+	{
+		.in = "foo@bar.example.com> text beyond",
+		.flags = 0,
+		.addr = "foo@bar.example.com",
+		.more = " text beyond",
+		.retval = 3
+	},
+	{
+		.in = NULL
+	}
+};
+
+static int 
+addrsyntax_test(void)
+{
+	int err = 0;
+	int i = 0;
+
+	while (syntaxpatterns[i].in != NULL) {
+		char inbuf[512];
+		string outaddr;
+		char *more = NULL;
+
+		STREMPTY(outaddr);
+		strcpy(inbuf, syntaxpatterns[i].in);
+
+		printf("Testing '%s' with flags %i\n", syntaxpatterns[i].in, syntaxpatterns[i].flags);
+
+		if (addrsyntax(inbuf, syntaxpatterns[i].flags, &outaddr, &more) != syntaxpatterns[i].retval) {
+			fprintf(stderr, "ERROR: did not return %i\n", syntaxpatterns[i].retval);
+			err++;
+			i++;
+			continue;
+		}
+
+		if ((outaddr.len != strlen(syntaxpatterns[i].addr)) ||
+				((outaddr.s != NULL) && (strcmp(outaddr.s, syntaxpatterns[i].addr) != 0))) {
+			fprintf (stderr, "ERROR: did not return address %s, but %s (lenght %lu)\n",
+					syntaxpatterns[i].addr, outaddr.s, (unsigned long)outaddr.len);
+			err++;
+		} else if ((more != NULL) && (syntaxpatterns[i].more) && strcmp(more, syntaxpatterns[i].more) != 0) {
+			fprintf (stderr, "ERROR: returned 'more' as %s instead of %s\n",
+					more, syntaxpatterns[i].more);
+			err++;
+		}
+
+		free(outaddr.s);
+
+		i++;
+	}
+
+	return err;
+}
 
 int
 main(void)
@@ -112,6 +230,8 @@ main(void)
 
 		i++;
 	}
+
+	err += addrsyntax_test();
 
 	return err;
 }
