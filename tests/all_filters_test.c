7ab68cfb1bbc1a11660fe156f75a97bce1f46b2e
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2012-02-04 16:34:53 +0000

    tests: simple fake configuration setup for All_Filters test
    svn path=/trunk/; revision=1374

--- tests/all_filters_test.c
+++ tests/all_filters_test.c
@@ -6,6 +6,7 @@
 #include "qsmtpd.h"
 
 #include <assert.h>
+#include <dirent.h>
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
@@ -31,12 +32,20 @@ dnstxt(char **a __attribute__ ((unused)), const char *b __attribute__ ((unused))
 }
 
 int
-main(void)
+main(int argc, char **argv)
 {
 	int i;
 	int err = 0;
 	struct userconf uc;
 	struct recip dummyrecip;
+	struct recip firstrecip;
+	DIR *basedir;
+	char confpath[PATH_MAX];
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <basedir>\n", argv[0]);
+		return EINVAL;
+	}
 
 	STREMPTY(uc.domainpath);
 	STREMPTY(uc.userpath);
@@ -52,6 +61,8 @@ main(void)
 	dummyrecip.to.len = strlen(dummyrecip.to.s);
 	dummyrecip.ok = 0;
 	TAILQ_INSERT_TAIL(&head, &dummyrecip, entries);
+
+	xmitstat.spf = SPF_IGNORE;
 
 	for (i = 0; rcpt_cbs[i] != NULL; i++) {
 		const char *errmsg;
@@ -70,12 +81,22 @@ main(void)
 	xmitstat.ipv4conn = 1; /* yes */
 	xmitstat.check2822 = 2; /* no decision yet */
 	xmitstat.helostatus = 1; /* HELO is my name */
+	xmitstat.spf = SPF_NONE;
 	xmitstat.fromdomain = 3; /* permanent error */
 	xmitstat.spacebug = 1; /* yes */
 	xmitstat.mailfrom.s = "user@invalid";
 	xmitstat.mailfrom.len = strlen(xmitstat.mailfrom.s);
 	xmitstat.helostr.s = "my.host.example.org";
 	xmitstat.helostr.len = strlen(xmitstat.helostr.s);
+
+	TAILQ_INIT(&head);
+
+	thisrecip = &dummyrecip;
+	firstrecip.to.s = "baz@example.com";
+	firstrecip.to.len = strlen(firstrecip.to.s);
+	firstrecip.ok = 0;
+	TAILQ_INSERT_TAIL(&head, &firstrecip, entries);
+	TAILQ_INSERT_TAIL(&head, &dummyrecip, entries);
 
 	for (i = 0; rcpt_cbs[i] != NULL; i++) {
 		const char *errmsg;
@@ -86,6 +107,59 @@ main(void)
 			fprintf(stderr, "filter %i returned %i\n", i, r);
 			err++;
 		}
+	}
+
+	xmitstat.esmtp = 0; /* no */
+
+	i = 0;
+	snprintf(confpath, sizeof(confpath), "%s/0/", argv[1]);
+	basedir = opendir(confpath);
+
+	while (basedir != NULL) {
+		char userpath[PATH_MAX];
+		int j;
+
+		closedir(basedir);
+		snprintf(userpath, sizeof(userpath), "%s/%i/user/", argv[1], i);
+		basedir = opendir(userpath);
+		if (basedir == NULL) {
+			uc.userpath.s = NULL;
+			uc.userpath.len = 0;
+		} else {
+			closedir(basedir);
+			uc.userpath.s = userpath;
+			uc.userpath.len = strlen(uc.userpath.s);
+		}
+
+		snprintf(confpath, sizeof(confpath), "%s/%i/domain/", argv[1], i);
+		basedir = opendir(confpath);
+		if (basedir == NULL) {
+			uc.domainpath.s = NULL;
+			uc.domainpath.len = 0;
+		} else {
+			closedir(basedir);
+			uc.domainpath.s = confpath;
+			uc.domainpath.len = strlen(uc.domainpath.s);
+		}
+
+		printf("testing configuration %i,%s%s\n", i,
+				uc.userpath.len ? " user" : "",
+				uc.domainpath.len ? " domain" : "");
+
+		for (j = 0; rcpt_cbs[j] != NULL; j++) {
+			const char *errmsg;
+			int bt = 0;
+			int r = rcpt_cbs[j](&uc, &errmsg, &bt);
+
+			if (r != 0) {
+				fprintf(stderr, "filter %i returned %i\n", j, r);
+				err++;
+			}
+		}
+
+		i++;
+		snprintf(confpath, sizeof(confpath), "%s/%i/", argv[1], i);
+		basedir = opendir(confpath);
 	}
 
 	return err;
