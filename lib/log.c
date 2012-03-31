c1acefc6d06fb5fc5ba4835de86466fd57a28a62
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2012-03-31 20:26:47 +0000

    log: do not write to stdout
    stdout is one of the network pipes of the process, writing log messages there will break things.
    
    svn path=/trunk/; revision=1390

--- lib/log.c
+++ lib/log.c
@@ -45,7 +45,7 @@ log_writen(int priority, const char **s)
 	syslog(priority, "%s", buf);
 #endif
 #ifndef NOSTDERR
-	write((priority > LOG_INFO) ? 2 : 1, buf, i);
+	write(2, buf, i);
 #endif
 	free(buf);
 }
