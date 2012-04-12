804e61a6f63519dda69286357089be92cf6c34bc
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2012-04-12 16:37:18 +0000

    tests: add testcase for Qsmtpd's date822()
    svn path=/trunk/; revision=1400

--- tests/qsdata_test.c
+++ tests/qsdata_test.c
@@ -1,6 +1,7 @@
 #include "../qsmtpd/data.c"
 
 #include <stdio.h>
+#include <time.h>
 
 int relayclient;
 unsigned long sslauth;
@@ -24,6 +25,15 @@ int submission_mode;
 struct recip *thisrecip;
 
 struct smtpcomm commands[8];
+
+// override this so always the same time is returned for testcases
+static time_t testtime;
+
+time_t
+time(time_t *t __attribute__ ((unused)))
+{
+	return testtime;
+}
 
 pid_t
 fork_clean()
@@ -53,7 +63,7 @@ spfreceived(int fd __attribute__ ((unused)), const int spf __attribute__ ((unuse
 }
 
 static int
-check_twodigit()
+check_twodigit(void)
 {
 	int ret = 0;
 	int i;
@@ -76,6 +86,36 @@ check_twodigit()
 	return ret;
 }
 
+static int
+check_date822(void)
+{
+	char buf[32];
+	const char *expt[] = { "Thu, 01 Jan 1970 00:00:00 +0000",
+			"Wed, 11 Apr 2012 18:32:17 +0200" };
+	const time_t testtimes[] = { 0, 1334161937 };
+	const char *tzones[] = { "TZ=UTC", "TZ=CET" };
+	int ret = 0;
+	int i;
+	char tzbuf[12];
+
+	memset(buf, 0, sizeof(buf));
+
+	for (i = 0; i < 2; i++) {
+		testtime = testtimes[i];
+		memcpy(tzbuf, tzones[i], strlen(tzones[i]) + 1);
+		putenv(tzbuf);
+		date822(buf);
+
+		if (strcmp(buf, expt[i])) {
+			ret++;
+			fprintf(stderr, "time %li was encoded to '%s' instead of '%s'\n",
+					(long) testtimes[i], buf, expt[i]);
+		}
+	}
+
+	return ret;
+}
+
 int main()
 {
 	int ret = 0;
@@ -83,6 +123,7 @@ int main()
 	memset(&xmitstat, 0, sizeof(xmitstat));
 
 	ret += check_twodigit();
+	ret += check_date822();
 
 	return ret;
 }
