fc5bbca902235109f808717b1124c01740b695d3
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2012-12-26 23:06:34 +0000

    doc: add documentation for TSPECIAL and WSPACE macros
    svn path=/trunk/; revision=1464

--- include/mime.h
+++ include/mime.h
@@ -15,9 +15,25 @@ extern size_t mime_param(const char *, const size_t) __attribute__ ((pure)) __at
 extern size_t mime_token(const char *, const size_t) __attribute__ ((pure)) __attribute__ ((nonnull(1)));
 extern off_t find_boundary(const char *, const off_t, const cstring *) __attribute__ ((pure)) __attribute__ ((nonnull (1,3)));
 
+/**
+ * check if the given character is a MIME special one
+ *
+ * @param a the character to check
+ * @return if it is a special character or not
+ *
+ * This checks if the given character is within the "tspecials" range as
+ * defined in RfC 2045.
+ */
 #define TSPECIAL(a) (((a) == '(') || ((a) == ')') || ((a) == '<') || ((a) == '>') || ((a) == '@') || \
 			((a) == ',') || ((a) == ';') || ((a) == ':') || ((a) == '\\') || ((a) == '"') || \
 			((a) == '/') || ((a) == '[') || ((a) == ']') || ((a) == '?') || ((a) == '='))
+
+/**
+ * check if the given character is whitespace
+ *
+ * @param a the character to check
+ * @return if it is a whitespace character or not
+ */
 #define WSPACE(a) (((a) == ' ') || ((a) == '\t') || ((a) == '\r') || ((a) == '\n'))
 
 #endif
