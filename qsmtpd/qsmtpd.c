ca2eb46855146131b2a8036f1f790c743b47718d
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2013-09-25 21:37:55 +0000

    Qsmtpd: fix copy of local IPv4 address when IPV4ONLY is defined
    svn path=/trunk/; revision=1496

--- qsmtpd/qsmtpd.c
+++ qsmtpd/qsmtpd.c
@@ -354,7 +354,7 @@ setup(void)
 		log_write(LOG_ERR, "can't figure out local IP (parse error)");
 		return 1;
 	}
-	memcpy(xmitstat.localip, tmp + 7, strlen(tmp + 7));
+	memcpy(xmitstat.localip, tmp, strlen(tmp));
 
 	tmp = getenv("TCPREMOTEIP");
 	if (!tmp || !*tmp) {
