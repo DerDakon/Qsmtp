4796748830ede1ef68b360bfccfbd646c406668b
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2013-10-04 20:55:44 +0000

    tests: add testcase for allocation failure in send_bdat()
    svn path=/trunk/; revision=1505

---
+++ tests/qrbdat_test.c
@@ -0,0 +1,114 @@
+#define _ISOC99_SOURCE
+#include "netio.h"
+#include "qrdata.h"
+#include "qremote.h"
+#include "test_io/testcase_io.h"
+
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+unsigned int may_log_count;
+
+string heloname;
+unsigned int smtpext;
+
+const char *msgdata;
+off_t msgsize;
+char *outbuf;
+size_t outlen;
+size_t outpos;
+
+#ifdef DEBUG_IO
+int in_data;
+#endif /* DEBUG_IO */
+
+void
+quit(void)
+{
+	fprintf(stderr, "%s() called unexpected\n", __FUNCTION__);
+	exit(EFAULT);
+}
+
+int
+checkreply(const char *status __attribute__ ((unused)), const char **pre __attribute__ ((unused)), const int mask __attribute__ ((unused)))
+{
+	fprintf(stderr, "%s was called but should not\n", __FUNCTION__);
+	exit(EFAULT);
+}
+
+static unsigned int was_send_data_called;
+
+void
+send_data(unsigned int recodeflag)
+{
+	if (recodeflag != 42) {
+		fprintf(stderr, "invalid recodeflag %u found\n", recodeflag);
+		exit(EINVAL);
+	}
+
+	was_send_data_called = 1;
+}
+
+void
+test_log_write(int priority, const char *s)
+{
+	if (may_log_count > 0) {
+		may_log_count--;
+		return;
+	}
+	fprintf(stderr, "log_write(%i, %s) called unexpected\n", priority, s);
+	exit(EFAULT);
+
+}
+
+void
+test_net_conn_shutdown(const enum conn_shutdown_type sdtype __attribute__((unused)))
+{
+	free(outbuf);
+	outbuf = NULL;
+}
+
+const char *successmsg[] = {"1", "2", "3", "4", "5", "6", "7", NULL};
+
+static int
+test_bad_malloc(void)
+{
+	may_log_count = 1;
+	chunksize = (size_t)-1;
+
+	testcase_setup_log_write(test_log_write);
+
+	msgdata = "1234\r\n.\r\n";
+	msgsize = strchr(msgdata, '\n') - msgdata + 1;
+
+	send_bdat(42);
+
+	if (may_log_count != 0) {
+		fprintf(stderr, "may_log_count is %i but should be 0\n", may_log_count);
+		return 1;
+	}
+
+	if (was_send_data_called != 1) {
+		fprintf(stderr, "send_data() should have been called once, but was called %u times\n", was_send_data_called);
+		return 1;
+	}
+
+	if (strcmp(successmsg[2], "3") != 0) {
+		fprintf(stderr, "successmsg[2] should have been '3', but is '%s'\n", successmsg[2]);
+		return 1;
+	}
+
+	return 0;
+}
+
+int main(void)
+{
+	int ret = 0;
+
+	ret += test_bad_malloc();
+
+	return ret;
+}
