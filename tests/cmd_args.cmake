0ad578d972d76a6cace72f406332f75b4fff86e3
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2013-11-19 18:03:18 +0000

    tests: add more cases of invalid SMTP command arguments
    svn path=/trunk/; revision=1558

--- tests/cmd_args.cmake
+++ tests/cmd_args.cmake
@@ -1,6 +1,11 @@
 cmake_minimum_required(VERSION 2.8)
 
 execute_process(COMMAND sleep 2)
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "HELO nonsense.example.org\r")
+execute_process(COMMAND sleep 1)
+# arguments only allowed in ESMTP mode
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> size=20\r")
+execute_process(COMMAND sleep 1)
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "EHLO nonsense.example.org\r")
 execute_process(COMMAND sleep 1)
 # invalid arguments
@@ -15,6 +20,14 @@ execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> size=20 size=20\r
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> size=20 body=7bit auth=foo@example.org\r")
 # argument after rcpt, which is not supported
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "rcpt to:<postmaster> invalid\r\nrset\r")
+execute_process(COMMAND sleep 1)
+# duplicate body, noop to reset bad command counter
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> body=7bit body=7bit\r\nnoop\r")
+execute_process(COMMAND sleep 1)
+# invalid size
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> size=a\r\nnoop\r")
+execute_process(COMMAND sleep 1)
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> size=2a\r\nnoop\r")
 execute_process(COMMAND sleep 1)
 # invalid body arguments, noop to reset bad command counter
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> body=foo\r\nnoop\r")
