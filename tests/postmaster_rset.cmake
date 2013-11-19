f365b2dc9d8f3c2866e238d26357ec45f52483f2
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2013-11-19 11:52:17 +0000

    tests: add a line with bad command arguments to "postmaster rset" test
    svn path=/trunk/; revision=1556

--- tests/postmaster_rset.cmake
+++ tests/postmaster_rset.cmake
@@ -9,6 +9,8 @@ execute_process(COMMAND ${CMAKE_COMMAND} -E echo "rSET\r")
 execute_process(COMMAND sleep 1)
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "EHLO nonsense.example.org\r")
 execute_process(COMMAND sleep 1)
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> foo=bar nonsense\r")
+execute_process(COMMAND sleep 1)
 # pipelining
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<>\r\nrcpt to:<postmaster>\r\nRset\r")
 execute_process(COMMAND sleep 1)
