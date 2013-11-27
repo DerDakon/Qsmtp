f5dd5eb01f53846dd1d6e7b631b2709184b51263
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2013-11-27 19:48:39 +0000

    tests: send DATA after bad bounce
    svn path=/trunk/; revision=1591

--- tests/bounce_multi_rcpt.cmake
+++ tests/bounce_multi_rcpt.cmake
@@ -4,7 +4,7 @@ execute_process(COMMAND sleep 2)
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "EHLO nonsense.example.org\r")
 execute_process(COMMAND sleep 1)
 # pipelining
-execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<>\r\nrcpt to:<postmaster>\r\nrcpt to:<postmaster>\r")
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<>\r\nrcpt to:<postmaster>\r\nrcpt to:<postmaster>\r\ndata\r")
 execute_process(COMMAND sleep 1)
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "quit\r")
 execute_process(COMMAND sleep 1)
