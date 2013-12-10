DerDakon/Qsmtp

commit 30101b7846e74a1f4000335e421f66f1ba6d861a

Qsmtpd: use newstr() in addrsyntax()
svn path=/trunk/; revision=1631

qsmtpd/addrsyntax.c
@@ -228,13 +228,11 @@ addrsyntax(char *in, const int flags, string *addr, char **more)
 	}
 
 	if (addr) {
-		addr->s = malloc(len + 1);
-		if (!addr->s)
+		if (newstr(addr, len + 1))
 			return -1;
 
 		strncpy(addr->s, f, len);
-		addr->s[len] = '\0';
-		addr->len = len;
+		addr->s[addr->len--] = '\0';
 		while (len > 0) {
 			len--;
 			if ((addr->s[len] >= 'A') && (addr->s[len] <= 'Z'))

commit 3d4e2e972a4f4348d41cdf9051bc408aaf407321

Qsmtpd: fix off-by-one in addrsyntax() introduced in r1631
svn path=/trunk/; revision=1633

qsmtpd/addrsyntax.c
@@ -232,7 +232,7 @@ addrsyntax(char *in, const int flags, string *addr, char **more)
 			return -1;
 
 		strncpy(addr->s, f, len);
-		addr->s[addr->len--] = '\0';
+		addr->s[--addr->len] = '\0';
 		while (len > 0) {
 			len--;
 			if ((addr->s[len] >= 'A') && (addr->s[len] <= 'Z'))