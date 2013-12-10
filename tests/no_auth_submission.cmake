c5da33e75dc0a20433e256f1ffa6c6401bc23fa8
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2013-12-10 19:26:53 +0000

    tests: add a testcase for unauthenticated mail on submission port
    svn path=/trunk/; revision=1632

---
+++ tests/no_auth_submission.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required(VERSION 2.8)
+
+execute_process(COMMAND sleep 2)
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "EHLO nonsense.example.org\r")
+execute_process(COMMAND sleep 1)
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<>\r")
+execute_process(COMMAND sleep 1)
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "quit\r")
+execute_process(COMMAND sleep 1)
