f7719bc4d5ea58aa4971857e6b1978e318134a2b
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2013-12-13 16:47:14 +0000

    tests: extend tests for syntax errors in MAIL FROM: and RCPT TO:
    svn path=/trunk/; revision=1651

--- tests/cmd_args.cmake
+++ tests/cmd_args.cmake
@@ -1,5 +1,13 @@
 cmake_minimum_required(VERSION 2.8)
 
+# The maximum permitted line length without extensions is 510 characters + CRLF.
+# The easiest way to have a otherwise valid line that violates this length limit
+# is to extend the space bug space.
+set(SPACES "    ")
+foreach(i 1 2 3 4 5 6 7)
+	set(SPACES "${SPACES}${SPACES}")
+endforeach(i)
+
 execute_process(COMMAND sleep 2)
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "HELO nonsense.example.org\r")
 execute_process(COMMAND sleep 1)
@@ -16,6 +24,12 @@ execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> size=20 body=8bit
 execute_process(COMMAND sleep 1)
 # duplicate size
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> size=20 size=20\r")
+# valid empty auth, valid body
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> auth=<> body=8bitmime\r")
+# space bug, rcpt to not in angle brackets
+# space bug, rcpt to address has syntax error
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "rcpt to: postmaster\r\nrcpt to: <foo@ju#k>\r\nrset\r")
+execute_process(COMMAND sleep 1)
 # valid size, another valid body, valid auth
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> size=20 body=7bit auth=foo@example.org\r")
 # argument after rcpt, which is not supported
@@ -27,7 +41,11 @@ execute_process(COMMAND sleep 1)
 # invalid size
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> size=a\r\nnoop\r")
 execute_process(COMMAND sleep 1)
-execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> size=2a\r\nnoop\r")
+# invalid size, space bug
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from: <> size=2a\r\nnoop\r")
+execute_process(COMMAND sleep 1)
+# missing angle brackets
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from: foo@example.net\r\nnoop\r")
 execute_process(COMMAND sleep 1)
 # invalid body arguments, noop to reset bad command counter
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<> body=foo\r\nnoop\r")
@@ -39,6 +57,12 @@ execute_process(COMMAND sleep 1)
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "noop foo\r\nnoop\r")
 execute_process(COMMAND sleep 1)
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "rset foo\r\nRSET\r")
+execute_process(COMMAND sleep 1)
+# too long line, noop to reset bad command counter
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:${SPACES}<>\r\nnoop\r")
+execute_process(COMMAND sleep 1)
+# mail address with syntax error, noop to reset bad command counter
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<foo@junk#>\r\nnoop\r")
 execute_process(COMMAND sleep 1)
 # invalid body arguments, noop, to reset bad command counter
 # pipelining after end of pipelining group
