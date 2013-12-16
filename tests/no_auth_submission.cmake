b859da9498a8f0e8acd8bfa0c29914dc0aea7f49
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2013-12-16 22:14:46 +0000

    tests: use IP address in brackets as EHLO in submission test
    svn path=/trunk/; revision=1659

--- tests/no_auth_submission.cmake
+++ tests/no_auth_submission.cmake
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 2.8)
 
 execute_process(COMMAND sleep 2)
-execute_process(COMMAND ${CMAKE_COMMAND} -E echo "EHLO nonsense.example.org\r")
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "EHLO [127.0.0.1]\r")
 execute_process(COMMAND sleep 1)
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<>\r")
 execute_process(COMMAND sleep 1)
