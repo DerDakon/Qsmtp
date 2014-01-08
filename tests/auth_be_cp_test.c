3aadea442943bdfe7a1d88a5bfec3f92939056a8
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2014-01-08 23:15:04 +0000

    tests: explicitely test auth_backend_setup() from checkpassword backend
    svn path=/trunk/; revision=1726

--- tests/auth_be_cp_test.c
+++ tests/auth_be_cp_test.c
@@ -9,6 +9,7 @@
 #include "auth_users.h"
 #include "test_io/testcase_io.h"
 
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -65,6 +66,56 @@ test_log_write(int priority, const char *s)
 	expected_log = NULL;
 }
 
+static int expect_log_writen;
+static char baddummy[PATH_MAX];
+
+void
+test_log_writen(int priority, const char **msg)
+{
+	const char *log_multi_expect[] = { "checkpassword program '", baddummy,
+			"' is not executable, error was: ",
+			strerror(ENOTDIR), NULL };
+	unsigned int c;
+
+	if (!expect_log_writen) {
+		int i;
+
+		fprintf(stderr, "unexpected call to %s(), args:\n", __func__);
+
+		for (i = 0; msg[i] != NULL; i++)
+			fprintf(stderr, "\t%s\n", msg[i]);
+		err++;
+		return;
+	} else {
+		expect_log_writen = 0;
+	}
+
+	if (priority != LOG_WARNING) {
+		fprintf(stderr, "log_writen(%i, ...) called, but expected priority is LOG_WARNING\n",
+				priority);
+		err++;
+	}
+
+	for (c = 0; msg[c] != NULL; c++) {
+		if (log_multi_expect[c] == NULL) {
+			fprintf(stderr, "log_writen(%i, ...) called, but expected parameter at position %u is NULL\n",
+					priority, c);
+			err++;
+		}
+		if (strcmp(log_multi_expect[c], msg[c]) != 0) {
+			fprintf(stderr, "log_writen(%i, ...) called, but parameter at position %u is '%s' instead of '%s'\n",
+					priority, c, msg[c], log_multi_expect[c]);
+			err++;
+		}
+	}
+
+	if (log_multi_expect[c] != NULL) {
+		fprintf(stderr, "log_writen(%i, ...) called, but expected parameter at position %u is not NULL, but %s\n",
+				priority, c, log_multi_expect[c]);
+		err++;
+	}
+}
+
 static void
 check_all_msgs(void)
 {
@@ -77,6 +128,11 @@ check_all_msgs(void)
 	if (expected_net_write != NULL) {
 		fprintf(stderr, "expected message '%s' was not received\n",
 				expected_net_write);
+		err++;
+	}
+
+	if (expect_log_writen) {
+		fprintf(stderr, "expected call to log_writen() was not received\n");
 		err++;
 	}
 }
@@ -135,9 +191,37 @@ test_chkpw_abort(void)
 	check_all_msgs();
 }
 
+/**
+ * @brief test auth_backend_setup() with invalid arguments
+ */
+static void
+test_setup_errors(const char *dummy)
+{
+	const char *args_invalid_count[] = { "Qsmtpd", "foo.example.com" };
+	const char *args_noexec[] = { "Qsmtpd", "foo.example.com", baddummy, "" };
+
+	strcpy(baddummy, dummy);
+	strcat(baddummy, "/something");
+
+	expected_log = "invalid number of parameters given";
+	if (auth_backend_setup(2, args_invalid_count) != -EINVAL) {
+		fprintf(stderr, "auth_backend_setup(2, ...) returned wrong error code\n");
+		err++;
+	}
+
+	expect_log_writen = 1;
+	if (auth_backend_setup(4, args_noexec) != -EACCES) {
+		fprintf(stderr, "auth_backend_setup(4, ...) returned wrong error code\n");
+		err++;
+	}
+
+	check_all_msgs();
+}
+
 int
 main(int argc, char **argv)
 {
+	const char *args[] = { "Qsmtpd", "foo.example.com", argv[1], autharg };
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s auth_dummy\n", argv[0]);
 		return 1;
@@ -147,6 +231,16 @@ main(int argc, char **argv)
 	testcase_setup_netnwrite(test_netnwrite);
 
 	test_fork_fail();
+
+	testcase_setup_log_writen(test_log_writen);
+
+	test_setup_errors(argv[1]);
+
+	if (auth_backend_setup(4, args) != 0) {
+		fprintf(stderr, "correct call to auth_backend_setup() failed\n");
+		return ++err;
+	}
+
 	test_chkpw_abort();
 
 	return err;
