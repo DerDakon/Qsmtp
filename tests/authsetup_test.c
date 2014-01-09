631fc93459dad0931b34f458cf2e9110d7bd84b9
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2014-01-09 20:41:31 +0000

    tests: simplify error reporting in AUTH_Setup test
    svn path=/trunk/; revision=1731

--- tests/authsetup_test.c
+++ tests/authsetup_test.c
@@ -63,15 +63,8 @@ check_authstr(const char *auth_expect)
 	}
 
 	if (strcmp(auth_str, auth_expect) != 0) {
-		const char msg1[] = "smtp_authstring() returned \"";
-		const char msg2[] = "\" instead of \"";
-		const char msg3[] = "\"\n";
-
-		write(2, msg1, strlen(msg1));
-		write(2, auth_str, strlen(auth_str));
-		write(2, msg2, strlen(msg2));
-		write(2, auth_expect, strlen(auth_expect));
-		write(2, msg3, strlen(msg3));
+		fprintf(stderr, "smtp_authstring() returned \"%s\" instead of \"%s\"\n",
+				auth_str, auth_expect);
 
 		free(auth_str);
 		return 1;
@@ -126,10 +119,8 @@ test_controlfiles(void)
 
 	while (patterns[idx].subdir != NULL) {
 		if (chdir(patterns[idx].subdir) != 0) {
-			const char *errmsg = "cannot chdir() to ";
-			write(2, errmsg, strlen(errmsg));
-			write(2, patterns[idx].subdir, strlen(patterns[idx].subdir));
-			write(2, "\n", 1);
+			fprintf(stderr, "cannot chdir(%s): %s\n",
+					patterns[idx].subdir, strerror(errno));
 			errcnt++;
 			idx++;
 			continue;
@@ -139,8 +130,8 @@ test_controlfiles(void)
 			errcnt++;
 
 		if (chdir("..") != 0) {
-			const char *errmsg = "cannot chdir() to back to start dir\n";
-			write(2, errmsg, strlen(errmsg));
+			fprintf(stderr, "cannot chdir(..): %s\n",
+					strerror(errno));
 			errcnt++;
 			idx++;
 			continue;
@@ -157,26 +148,22 @@ test_nonexistent(void)
 	char *auth_str;
 
 	if (chdir("nonexistent") != 0) {
-		const char *errmsg = "cannot chdir() to \"nonexistent\"\n";
-		write(2, errmsg, strlen(errmsg));
+		fputs("cannot chdir() to \"nonexistent\"\n", stderr);
 		return 1;
 	}
 
 	auth_str = smtp_authstring();
 
 	if (auth_str != NULL) {
-		const char errmsg1[] = "smtp_authstring() returned \"";
-		const char errmsg2[] = "\" but is should have returned NULL\n";
-		write(2, errmsg1, strlen(errmsg1));
-		write(2, auth_str, strlen(auth_str));
-		write(2, errmsg2, strlen(errmsg2));
+		fprintf(stderr, "smtp_authstring() returned \"%s\" but is should have returned NULL\n",
+				auth_str);
 		free(auth_str);
 		return 1;
 	}
 
 	if (chdir("..") != 0) {
-		const char *errmsg = "cannot chdir() to back to start dir\n";
-		write(2, errmsg, strlen(errmsg));
+		fprintf(stderr, "cannot chdir(..): %s\n",
+				strerror(errno));
 		return 1;
 	}
 
@@ -326,16 +313,14 @@ int main(int argc, char **argv)
 	testcase_ignore_log_writen();
 
 	if (argc != 2) {
-		const char *errmsg = "required argument missing: base directory for control file tests\n";
-		write(2, errmsg, strlen(errmsg));
+		fputs("required argument missing: base directory for control file tests\n", stderr);
 		return EINVAL;
 	}
 
 	auth_setup(3, argv_auth);
 
 	if (chdir(argv[1]) != 0) {
-		const char *errmsg = "cannot chdir() to given directory\n";
-		write(2, errmsg, strlen(errmsg));
+		fprintf(stderr, "cannot chdir(%s): %s\n", argv[1], strerror(errno));
 		return -1;
 	}
 
