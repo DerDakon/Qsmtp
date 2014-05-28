5cbc601de5124fa3ca03f8903b1a90e965a965dc
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2014-05-28 20:18:16 +0000

    tests: add testcase for invalid MIME boundaries that terminate the process
    svn path=/trunk/; revision=1922

---
+++ tests/mime_boundary_test.c
@@ -0,0 +1,47 @@
+#include <qremote/mime.h>
+#include <netio.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+void
+write_status(const char *str)
+{
+	puts(str);
+}
+
+void
+net_conn_shutdown(const enum conn_shutdown_type sd_type)
+{
+	if (sd_type == shutdown_abort)
+		exit(0);
+	else
+		exit(EINVAL);
+}
+
+int
+main(int argc, char **argv)
+{
+	char buf[256];
+	int r;
+	cstring line, boundary;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s boundary\n", argv[0]);
+		return -1;
+	}
+
+	snprintf(buf, sizeof(buf), "Content-Type: multipart/mixed; boundary=%s", argv[1]);
+
+	line.s = buf;
+	line.len = strlen(line.s);
+
+	r = is_multipart(&line, &boundary);
+
+	fprintf(stderr, "is_multipart() returned %i\n", r);
+
+	/* this test should be used for the invalid cases that terminate the
+	 * process, use mime_test.c for the other ones. */
+	return -1;
+}
