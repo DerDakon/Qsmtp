26262b03131264195c47390f3fc32aa69454e131
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2014-06-16 20:34:41 +0000

    tests: add another invalid write test for queue_envelope()
    svn path=/trunk/; revision=1965

--- tests/queue_envelope_test.c
+++ tests/queue_envelope_test.c
@@ -65,12 +65,45 @@ test_invalid_write(void)
 	}
 
 	if (queuefd_data != -1) {
-		fprintf(stderr, "queue_envelope() did not close queuefd_data\n");
+		fprintf(stderr, "queue_envelope() did not reset queuefd_data\n");
 		ret++;
 	}
 
 	if (queuefd_hdr != -1) {
-		fprintf(stderr, "queue_envelope() did not close queuefd_data\n");
+		fprintf(stderr, "queue_envelope() did not reset queuefd_hdr\n");
+		ret++;
+	}
+
+	return ret;
+}
+
+static int
+test_invalid_write2(void)
+{
+	int ret = 0;
+	int r;
+
+	queuefd_data = open(".", O_RDONLY);
+	queuefd_hdr = -1;
+
+	goodrcpt = 1;
+
+	errno = 0;
+	r = queue_envelope(0, 0);
+
+	if ((r != -1) || (errno != EBADF)) {
+		fprintf(stderr, "calling queue_envelope() with invalid returned %i/%i\n",
+			r, errno);
+		ret++;
+	}
+
+	if (queuefd_data != -1) {
+		fprintf(stderr, "queue_envelope() did not reset queuefd_data\n");
+		ret++;
+	}
+
+	if (queuefd_hdr != -1) {
+		fprintf(stderr, "queue_envelope() did not reset queuefd_hdr\n");
 		ret++;
 	}
 
@@ -84,6 +117,7 @@ main(void)
 
 	ret += test_invalid_close();
 	ret += test_invalid_write();
+	ret += test_invalid_write2();
 
 	return ret;
 }
