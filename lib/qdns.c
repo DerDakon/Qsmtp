eb37c00b2e664c9bf778b42c7362e0119e1c76a8
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2014-07-26 20:21:15 +0000

    lib: reformat DNS functions
    This has no logic change, only whitespace. Invert the first if condition in those functions to save some
    indentation.
    
    svn path=/trunk/; revision=2073

--- lib/qdns.c
+++ lib/qdns.c
@@ -27,81 +27,89 @@ ask_dnsmx(const char *name, struct ips **result)
 	int i;
 	char *r;
 	size_t l;
+	char *s;
+	struct ips **q = result;
+	int errtype = 0;
 
 	i = dnsmx(&r, &l, name);
 
-	if (!i || ((i < 0) && (errno == ENOENT)) ) {
-		char *s = r;
-		struct ips **q = result;
-		int errtype = 0;
-
-		/* there is no MX record, so we look for an AAAA record */
-		if (!l) {
-			int rc = ask_dnsaaaa(name, result);
-
-			if (!rc) {
-				struct ips *a = *result;
-
-				while (a) {
-					/* the DNS priority is 2 bytes long so 65536 can
-					   never be returned from a real DNS_MX lookup */
-					a->priority = 65536;
-					a = a->next;
-				}
-			}
-			return rc;
+	if ((i != 0) && (errno != ENOENT)) {
+		switch (errno) {
+		case ETIMEDOUT:
+		case EAGAIN:
+			return 2;
+		case ENFILE:
+		case EMFILE:
+		case ENOBUFS:
+			errno = ENOMEM;
+			/* fallthrough */
+		case ENOMEM:
+			return -1;
+		case ENOENT:
+			return 1;
+		default:
+			return 3;
 		}
+	}
 
-		while (r + l > s) {
-			struct ips *p;
-			int rc;
+	s = r;
 
-			rc = ask_dnsaaaa(s + 2, &p);
-			if (rc < 0) {
-				freeips(*result);
-				free(r);
-				if (errno == ENOMEM)
-					return -1;
-				return 2;
-			} else if (!rc) {
-				struct ips *u;
-				unsigned int pri = ntohs(*((unsigned short *) s));
+	/* there is no MX record, so we look for an AAAA record */
+	if (!l) {
+		int rc = ask_dnsaaaa(name, result);
 
-				/* add the new results to the list */
-				*q = p;
-				/* set priority for each of the new entries */
-				for (u = p; u; u = p->next) {
-					u->priority = pri;
-					p = u;
-				}
-				q = &(p->next);
-			} else {
-				errtype = (1 << rc);
-			}
-			s += 3 + strlen(s + 2);
-		}
-		free(r);
-		if (!*result) {
-			if (errtype & 4) {
-				return 2;
-			} else if (errtype & 2) {
-				return 1;
-			} else {
-				return 3;
+		if (!rc) {
+			struct ips *a = *result;
+
+			while (a) {
+				/* the DNS priority is 2 bytes long so 65536 can
+					never be returned from a real DNS_MX lookup */
+				a->priority = 65536;
+				a = a->next;
 			}
 		}
+		return rc;
+	}
+
+	while (r + l > s) {
+		struct ips *p;
+		int rc;
+
+		rc = ask_dnsaaaa(s + 2, &p);
+		if (rc < 0) {
+			freeips(*result);
+			free(r);
+			if (errno == ENOMEM)
+				return -1;
+			return 2;
+		} else if (!rc) {
+			struct ips *u;
+			unsigned int pri = ntohs(*((unsigned short *) s));
+
+			/* add the new results to the list */
+			*q = p;
+			/* set priority for each of the new entries */
+			for (u = p; u; u = p->next) {
+				u->priority = pri;
+				p = u;
+			}
+			q = &(p->next);
+		} else {
+			errtype = (1 << rc);
+		}
+		s += 3 + strlen(s + 2);
+	}
+
+	free(r);
+
+	if (*result)
 		return 0;
-	}
-	switch (errno) {
-	case ETIMEDOUT:
-	case EAGAIN:	return 2;
-	case ENFILE:
-	case EMFILE:
-	case ENOBUFS:	errno = ENOMEM;
-	case ENOMEM:	return -1;
-	case ENOENT:	return 1;
-	default:	return 3;
-	}
+	else if (errtype & 4)
+		return 2;
+	else if (errtype & 2)
+		return 1;
+	else
+		return 3;
 }
 
 /**
@@ -121,45 +129,55 @@ ask_dnsaaaa(const char *name, struct ips **result)
 	int i;
 	char *r;
 	size_t l;
+	char *s;
+	struct ips **q = result;
+
+	*result = NULL;
 
 	i = dnsip6(&r, &l, name);
-	if (!i) {
-		char *s = r;
-		struct ips **q = result;
-		*result = NULL;
-
-		if (!l)
-			return 1;
-
-		while (r + l > s) {
-			struct ips *p = malloc(sizeof(*p));
-
-			if (!p) {
-				freeips(*result);
-				free(r);
-				return -1;
-			}
-			*q = p;
-			p->next = NULL;
-			memcpy(&(p->addr), s, 16);
-
-			q = &(p->next);
-			s += 16;
-		}
+	if (i < 0) {
 		free(r);
-		return 0;
+		switch (errno) {
+		case ETIMEDOUT:
+		case EAGAIN:
+			return 2;
+		case ENFILE:
+		case EMFILE:
+		case ENOBUFS:
+			errno = ENOMEM;
+			/* fallthrough */
+		case ENOMEM:
+			return -1;
+		case ENOENT:
+			return 1;
+		default:
+			return 3;
+		}
 	}
+
+	s = r;
+
+	if (!l)
+		return 1;
+
+	while (r + l > s) {
+		struct ips *p = malloc(sizeof(*p));
+
+		if (!p) {
+			freeips(*result);
+			free(r);
+			return -1;
+		}
+		*q = p;
+		p->next = NULL;
+		memcpy(&(p->addr), s, 16);
+
+		q = &(p->next);
+		s += 16;
+	}
+
 	free(r);
-	switch (errno) {
-	case ETIMEDOUT:
-	case EAGAIN:	return 2;
-	case ENFILE:
-	case EMFILE:
-	case ENOBUFS:	errno = ENOMEM;
-	case ENOMEM:	return -1;
-	case ENOENT:	return 1;
-	default:	return 3;
-	}
+	return 0;
 }
 
 /**
@@ -181,47 +199,56 @@ ask_dnsa(const char *name, struct ips **result)
 	size_t l;
 
 	i = dnsip4(&r, &l, name);
-	if (!i) {
-		if (result) {
-			char *s = r;
-			struct ips **q = result;
-
-			if (!l) {
-				*result = NULL;
-				return 1;
-			}
-			while (r + l > s) {
-				struct ips *p = malloc(sizeof(*p));
-
-				if (!p) {
-					freeips(*result);
-					free(r);
-					return -1;
-				}
-				*q = p;
-				p->next = NULL;
-				p->addr.s6_addr32[0] = 0;
-				p->addr.s6_addr32[1] = 0;
-				p->addr.s6_addr32[2] = htonl(0xffff);
-				memcpy(&(p->addr.s6_addr32[3]), s, 4);
-
-				q = &(p->next);
-				s += 4;
-			}
+	if (i < 0) {
+		switch (errno) {
+		case ETIMEDOUT:
+		case EAGAIN:
+			return 2;
+		case ENFILE:
+		case EMFILE:
+		case ENOBUFS:
+			errno = ENOMEM;
+			/* fallthrough */
+		case ENOMEM:
+			return -1;
+		case ENOENT:
+			return 1;
+		default:
+			return 3;
 		}
-		free(r);
-		return l ? 0 : 1;
 	}
-	switch (errno) {
-	case ETIMEDOUT:
-	case EAGAIN:	return 2;
-	case ENFILE:
-	case EMFILE:
-	case ENOBUFS:	errno = ENOMEM;
-	case ENOMEM:	return -1;
-	case ENOENT:	return 1;
-	default:	return 3;
+
+	if (result) {
+		char *s = r;
+		struct ips **q = result;
+
+		if (!l) {
+			*result = NULL;
+			return 1;
+		}
+
+		while (r + l > s) {
+			struct ips *p = malloc(sizeof(*p));
+
+			if (!p) {
+				freeips(*result);
+				free(r);
+				return -1;
+			}
+			*q = p;
+			p->next = NULL;
+			p->addr.s6_addr32[0] = 0;
+			p->addr.s6_addr32[1] = 0;
+			p->addr.s6_addr32[2] = htonl(0xffff);
+			memcpy(&(p->addr.s6_addr32[3]), s, 4);
+
+			q = &(p->next);
+			s += 4;
+		}
 	}
+
+	free(r);
+	return l ? 0 : 1;
 }
 
 /**
@@ -241,8 +268,10 @@ ask_dnsname(const struct in6_addr *ip, char **result)
 	int r;
 
 	r = dnsname(result, ip);
+
 	if (!r)
 		return *result ? 1 : 0;
+
 	switch (errno) {
 	case ETIMEDOUT:
 	case EAGAIN:
