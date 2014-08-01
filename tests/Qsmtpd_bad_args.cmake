79236d1c707be9ff9bf8e93ec24bcb7312b8b7ba
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2014-08-01 19:16:42 +0000

    tests: execute some antispam codepaths when testing Qsmtpd
    svn path=/trunk/; revision=2116

--- tests/Qsmtpd_bad_args.cmake
+++ tests/Qsmtpd_bad_args.cmake
@@ -13,5 +13,11 @@ execute_process(COMMAND ${CMAKE_COMMAND} -E echo "noop foo\r\nnoop\r")
 execute_process(COMMAND sleep 1)
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "rset foo\r\nRSET\r")
 execute_process(COMMAND sleep 1)
+# the next 2 are syntactically correct, but should trigger some antispam codepath internally
+# TODO: move to their own test where those result codes are actually checked
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "helo 127.0.0.1\r")
+execute_process(COMMAND sleep 1)
+execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<someone@example.invalid>\r")
+execute_process(COMMAND sleep 1)
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "quit\r")
 execute_process(COMMAND sleep 1)
