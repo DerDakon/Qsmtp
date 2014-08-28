0b2d14ffcb227e5529c6bffa1f4da736dae8ce32
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2014-08-28 16:43:21 +0000

    tests: allow filter_SPF test to specify the config values instead of hardcoding them
    svn path=/trunk/; revision=2247

--- tests/filter_spf.c
+++ tests/filter_spf.c
@@ -30,34 +30,62 @@ enum filter_result cb_forceesmtp(const struct userconf *d __attribute__ ((unused
 enum filter_result cb_namebl(const struct userconf *d __attribute__ ((unused)), const char **l __attribute__ ((unused)), enum config_domain *t __attribute__ ((unused))) { abort(); }
 enum filter_result cb_wildcardns(const struct userconf *d __attribute__ ((unused)), const char **l __attribute__ ((unused)), enum config_domain *t __attribute__ ((unused))) { abort(); }
 
-long
-getsetting(const struct userconf *ds __attribute__ ((unused)), const char *c, enum config_domain  *t)
+/* This test (ab)uses the fields in struct userconf to set the expected
+ * results of getsetting() and getsettingglobal().
+ *
+ * userdirfd & 0xff00   -> config domain (must only be user, domain, or none)
+ * userdirfd & 0x0001   -> return value for getsetting(ds, "fail_hard_on_temp", t)
+ *
+ * domaindirfd & 0xff00 -> config domain (all values permitted)
+ * domaindirfd & 0x00ff -> return value for getsettingglobal(ds, "spfpolicy", t)
+ */
+
+static enum config_domain
+decode_config_domain(const int i)
 {
+	const int t = (i & 0xff00) >> 8;
+
+	switch (t) {
+	case CONFIG_DOMAIN:
+	case CONFIG_USER:
+	case CONFIG_GLOBAL:
+		return t;
+	default:
+		assert(i == 0);
+		return CONFIG_NONE;
+	}
+}
+
+long
+getsetting(const struct userconf *ds, const char *c, enum config_domain  *t)
+{
+	int i;
 	assert(strcmp(c, "fail_hard_on_temp") == 0);
 
-	*t = CONFIG_NONE;
+	*t = decode_config_domain(ds->userdirfd);
+	assert(*t != CONFIG_GLOBAL);
 
-	return 0;
+	i = (ds->userdirfd & 0xff);
+	((struct userconf *)ds)->userdirfd = 0;
+
+	return i;
 }
 
 long
 getsettingglobal(const struct userconf *ds, const char *c, enum config_domain *t)
 {
+	int i;
 	assert(strcmp(c, "spfpolicy") == 0);
 
 	if (ds == NULL)
 		return 0;
 
-	if (ds->userdirfd > 0) {
-		*t = CONFIG_USER;
-		return ds->userdirfd;
-	} else if (ds->domaindirfd > 0) {
-		*t = CONFIG_DOMAIN;
-		return ds->domaindirfd;
-	} else {
-		*t = CONFIG_GLOBAL;
-		return 1;
-	}
+	*t = decode_config_domain(ds->domaindirfd);
+
+	i = (ds->domaindirfd & 0xff);
+	((struct userconf *)ds)->domaindirfd = 0;
+
+	return i;
 }
 
 int
@@ -130,18 +158,23 @@ main(void)
 
 		const unsigned int spf:4;		/* the SPF status to test */
 		const unsigned int use_rcpt:1;	/* set thisrecip */
+
 		const unsigned int no_params:1;	/* pass NULL values for all parameters */
+		const unsigned char spfpolicy;	/* config value for spfpolicy */
+		const unsigned char tempsetting;	/* config value for fail_hard_on_temp */
+
+		const enum config_domain cd_policy;	/* domain for spfpolicy */
+		const enum config_domain cd_temp;	/* domain for fail_hard_on_temp */
 
 		/* output */
 		const int expected_result;
-
 		const int expected_errno;
-		const enum config_domain expected_t;
 
 		const char *expected_logmsg;
 		const char *expected_netmsg;
 		const char *expected_syslogmsg;
 
+		const enum config_domain expected_t;
 		const int expected_syslogprio;
 	} testpatterns[] = {
 		{
@@ -165,6 +198,8 @@ main(void)
 		{
 			.name = "spf == SPF_TEMP_ERROR",
 			.spf = SPF_TEMP_ERROR,
+			.spfpolicy = 1,
+			.cd_policy = CONFIG_GLOBAL,
 			.expected_result = FILTER_DENIED_WITH_MESSAGE,
 			.expected_logmsg = "temp SPF",
 			.expected_netmsg = "451 4.4.3 temporary error when checking the SPF policy\r\n",
@@ -174,6 +209,8 @@ main(void)
 			.name = "error in userconf_find_domain()",
 			.remotehost = hostname_spfignore_fail,
 			.spf = SPF_TEMP_ERROR,
+			.spfpolicy = 1,
+			.cd_policy = CONFIG_USER,
 			.expected_result = FILTER_ERROR,
 			.expected_errno = ENOTBLK
 		},
@@ -182,6 +219,8 @@ main(void)
 			.remotehost = hostname_spfignore,
 			.spf = SPF_TEMP_ERROR,
 			.use_rcpt = 1,
+			.spfpolicy = 1,
+			.cd_policy = CONFIG_GLOBAL,
 			.expected_result = FILTER_PASSED,
 			.expected_syslogmsg = "not rejected message to <someone@example.org> from <> from IP [] {SPF blocked by global policy, whitelisted by user policy}",
 			.expected_syslogprio = LOG_INFO
@@ -190,6 +229,8 @@ main(void)
 			.name = "spf == SPF_SOFTFAIL",
 			.spf = SPF_SOFTFAIL,
 			.helo = "example.net",
+			.spfpolicy = 1,
+			.cd_policy = CONFIG_USER,
 			.expected_result = FILTER_PASSED
 		},
 		{
@@ -197,6 +238,8 @@ main(void)
 			.remotehost = hostname_spfstrict,
 			.mailfrom = "someone@spfstrict.example.com",
 			.spf = SPF_SOFTFAIL,
+			.spfpolicy = 1,
+			.cd_policy = CONFIG_DOMAIN,
 			.expected_result = FILTER_DENIED_WITH_MESSAGE,
 			.expected_netmsg = "550 5.7.1 mail denied by SPF policy\r\n",
 			.expected_logmsg = "SPF",
@@ -242,6 +285,15 @@ main(void)
 			thisrecip = &rcpt;
 		else
 			thisrecip = NULL;
+
+		if (testpatterns[i].tempsetting == 0)
+			ds.userdirfd = 0;
+		else
+			ds.userdirfd = (testpatterns[i].cd_temp << 8) | testpatterns[i].tempsetting;
+		if (testpatterns[i].spfpolicy == 0)
+			ds.domaindirfd = 0;
+		else
+			ds.domaindirfd = (testpatterns[i].cd_policy << 8) | testpatterns[i].spfpolicy;
 
 		printf("testing: %s\n", testpatterns[i].name);
 
@@ -294,6 +346,16 @@ main(void)
 		if (log_write_msg != NULL) {
 			fprintf(stderr, "test %s: expected syslog message '%s' was not sent\n",
 					testpatterns[i].name, testpatterns[i].expected_syslogmsg);
+			err++;
+		}
+		if (ds.domaindirfd != 0) {
+			fprintf(stderr, "test %s: expected call to getsettingglobal() did not happen\n",
+					testpatterns[i].name);
+			err++;
+		}
+		if (ds.userdirfd != 0) {
+			fprintf(stderr, "test %s: expected call to getsettingglobal() did not happen\n",
+					testpatterns[i].name);
 			err++;
 		}
 	}
