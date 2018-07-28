d3c4681f497e2d1aeef3ba9390875843d7c6d5a0
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2018-07-28 15:37:19 +0200

    Qremote: use writev() to write status messages

--- qremote/status.c
+++ qremote/status.c
@@ -6,11 +6,24 @@
 #include <netio.h>
 
 #include <string.h>
+#include <sys/uio.h>
 #include <unistd.h>
 
 /* intentionally not in any header file, noone (but the testcases) should ever
  * need to access this. */
 int statusfd = 1;
+
+static void
+write_status_vec(const struct iovec *data, int cnt)
+{
+	ssize_t slen = 0;
+	for (int i = 0; i < cnt; i++)
+		slen += data[i].iov_len;
+
+	/* see write_status_raw() for reasoning */
+	if (writev(statusfd, data, cnt) != slen)
+		net_conn_shutdown(shutdown_clean);
+}
 
 void
 write_status_raw(const char *str, const size_t len)
@@ -19,21 +32,45 @@ write_status_raw(const char *str, const size_t len)
 	 * the process. Worst case is that the mail was successfully sent but
 	 * this can't be recorded, in which case the mail will just be sent
 	 * again. */
-	if (write(statusfd, str, len) < 0)
+	if (write(statusfd, str, len) != (ssize_t)len)
 		net_conn_shutdown(shutdown_clean);
 }
 
 void
 write_status(const char *str)
 {
-	write_status_raw(str, strlen(str));
-	write_status_raw("\n", 2);
+	struct iovec data[] = {
+		{
+			.iov_base = (void*)str,
+			.iov_len = strlen(str)
+		},
+		{
+			.iov_base = "\n",
+			.iov_len = 2
+		}
+	};
+
+	write_status_vec(data, 2);
 }
 
 void
 write_status_m(const char **strs, const unsigned int count)
 {
-	for (unsigned int i = 0; i < count - 1; i++)
-		write_status_raw(strs[i], strlen(strs[i]));
-	write_status(strs[count - 1]);
+	struct iovec *vectors = calloc(count + 1, sizeof(*vectors));
+
+	if (vectors == NULL) {
+		// fallback, less efficient
+		for (unsigned int i = 0; i < count - 1; i++)
+			write_status_raw(strs[i], strlen(strs[i]));
+		write_status(strs[count - 1]);
+	} else {
+		for (unsigned int i = 0; i < count; i++) {
+			vectors[i].iov_base = (void*)strs[i];
+			vectors[i].iov_len = strlen(strs[i]);
+		}
+		vectors[count].iov_base = "\n";
+		vectors[count].iov_len = 2;
+		write_status_vec(vectors, count + 1);
+		free(vectors);
+	}
 }
