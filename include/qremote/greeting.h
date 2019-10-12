1d8844edbad4d14a5d9f3cc4a4564704152bcdfd
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2019-10-12 13:45:08 +0200

    fix esmtp_x_final marker
    Fixes: 7c544fbaf8ef6bdd122d114d03164c0e48112ac5

--- include/qremote/greeting.h
+++ include/qremote/greeting.h
@@ -12,7 +12,7 @@ enum ehlo_extensions {
 	esmtp_8bitmime = 0x8,	/**< 8BITMIME (RfC 6152) */
 	esmtp_auth = 0x10,	/**< AUTH (RfC 2554) */
 	esmtp_chunking = 0x20,	/**< CHUNKING (RfC 3030) */
-	esmtp_x_final = esmtp_auth/**< end delimiter */
+	esmtp_x_final = esmtp_chunking/**< end delimiter */
 };
 
 extern unsigned long remotesize;	/**< the maximum size allow by the remote host or 0 if unlimited or unknown */
