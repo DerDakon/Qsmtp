46c0115a5df3f271ebc484a7c92b8eddbd8448f0
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2020-06-20 20:26:18 +0200

    CDash: upload with https

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -7,7 +7,7 @@
 set(CTEST_PROJECT_NAME "Qsmtp")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
 
-set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=Qsmtp")
 set(CTEST_DROP_SITE_CDASH TRUE)
