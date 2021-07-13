698816a3ae8483dbfe2826739beb0f5b6bdcc7a4
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2021-07-13 18:45:43 +0200

    tests: uncommentt everything in cert generation script

--- tests/ssl_pp/testcerts.sh
+++ tests/ssl_pp/testcerts.sh
@@ -2,21 +2,21 @@
 
 KEYSIZE=4096
 
-# openssl req -x509 -newkey rsa:${KEYSIZE} -keyout valid${KEYSIZE}.key \
-# 	-out valid${KEYSIZE}.crt -days 10000 -nodes \
-# 	-subj '/C=DE/ST=Some-State/O=Qsmtp testcases/CN=testcert.example.org'
-#
-# openssl req -x509 -newkey rsa:${KEYSIZE} -keyout valid${KEYSIZE}_san.key \
-# 	-out valid${KEYSIZE}_san.crt -days 10000 -nodes \
-# 	-subj '/C=DE/ST=Some-State/O=Qsmtp testcases' -addext 'subjectAltName = DNS:testcert.example.org'
-#
-# openssl req -x509 -newkey rsa:${KEYSIZE} -keyout wildcard${KEYSIZE}.key \
-# 	-out wildcard${KEYSIZE}.crt -days 10000 -nodes \
-# 	-subj '/C=DE/ST=Some-State/O=Qsmtp testcases/CN=*.example.org'
-#
-# openssl req -x509 -newkey rsa:${KEYSIZE} -keyout expired.key \
-# 	-out expired.crt -days 1 -nodes \
-# 	-subj '/C=DE/ST=Some-State/O=Qsmtp testcases/CN=testcert.example.org'
+openssl req -x509 -newkey rsa:${KEYSIZE} -keyout valid${KEYSIZE}.key \
+	-out valid${KEYSIZE}.crt -days 10000 -nodes \
+	-subj '/C=DE/ST=Some-State/O=Qsmtp testcases/CN=testcert.example.org'
+
+openssl req -x509 -newkey rsa:${KEYSIZE} -keyout valid${KEYSIZE}_san.key \
+	-out valid${KEYSIZE}_san.crt -days 10000 -nodes \
+	-subj '/C=DE/ST=Some-State/O=Qsmtp testcases' -addext 'subjectAltName = DNS:testcert.example.org'
+
+openssl req -x509 -newkey rsa:${KEYSIZE} -keyout wildcard${KEYSIZE}.key \
+	-out wildcard${KEYSIZE}.crt -days 10000 -nodes \
+	-subj '/C=DE/ST=Some-State/O=Qsmtp testcases/CN=*.example.org'
+
+openssl req -x509 -newkey rsa:${KEYSIZE} -keyout expired.key \
+	-out expired.crt -days 1 -nodes \
+	-subj '/C=DE/ST=Some-State/O=Qsmtp testcases/CN=testcert.example.org'
 
 openssl req -x509 -newkey rsa:${KEYSIZE} -keyout valid${KEYSIZE}_san2.key \
 	-out valid${KEYSIZE}_san2.crt -days 10000 -nodes \
