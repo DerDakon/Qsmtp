441d13efea61b58afe1c4e59262e081dbe6d641b
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2021-07-16 14:29:59 +0200

    tests: use CMake portable sleep command for QUIT tests

--- tests/quit.cmake
+++ tests/quit.cmake
@@ -1,5 +1,5 @@
 cmake_minimum_required(VERSION 3.0)
 
-execute_process(COMMAND sleep 4)
+execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 4)
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "quit\r")
-execute_process(COMMAND sleep 1)
+execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 1)
