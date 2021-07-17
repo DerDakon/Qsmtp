047369882d46c5f4739bdc7f1543b9ba7d807922
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2021-07-17 10:38:26 +0200

    tests: prolong timeout for quit_no_ips test
    We must wait long enough for the DNS query to fail.

--- tests/quit.cmake
+++ tests/quit.cmake
@@ -1,5 +1,5 @@
 cmake_minimum_required(VERSION 3.0)
 
-execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 4)
+execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 15) # long enough for DNS query to time out
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "quit\r")
 execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 1)
