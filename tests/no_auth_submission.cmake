2adde4da2b8f3a39021f77ac25fa3de075481c2c
Rolf Eike Beer <qsmtp@opensource.sf-tec.de>
2021-08-18 17:38:19 +0200

    tests: prolong initial delay also for Qsmtpd_no_auth_submission test

--- tests/no_auth_submission.cmake
+++ tests/no_auth_submission.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.0)
 
-execute_process(COMMAND sleep 3)
+execute_process(COMMAND sleep 15)
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "EHLO [203.0.113.24]\r")
 execute_process(COMMAND sleep 1)
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "mail from:<>\r")
